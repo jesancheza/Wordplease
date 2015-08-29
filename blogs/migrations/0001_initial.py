b5025a1780e5bee7dc15db494254682511c78ccd
Jose Enrique Sanchez Alfonso <jesanchez@jesanchez.es>
2015-08-29 16:39:40 +0200

    Creado primer modelo de blog Carga simple de todos los blog que existan en la bbdd

---
+++ blogs/migrations/0001_initial.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Blog',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('modified_at', models.DateTimeField(auto_now=True)),
+            ],
+        ),
+    ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,14 @@
+#-*- coding: utf-8 -*-
+from blogs.models import Blog
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    blogs = Blog.objects.all()
+
+    html = "<ul>"
+    for blog in blogs:
+        html += "<li> Blog de " + +"</li>"
+
+    html += "</ul>"
+    return HttpResponse(html)
