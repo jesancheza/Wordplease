3d2aee570020e3fda38dcce1105b2f855e765029
Jose Enrique Sanchez Alfonso <jesanchez@jesanchez.es>
2015-08-30 13:29:39 +0200

    Añadido título y descripción al blog

--- blogs/models.py
+++ blogs/models.py
@@ -1,8 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
 
 class Blog(models.Model):
 
     owner = models.OneToOneField(User)
+    title = models.TextField(max_length=150)
+    description = models.TextField(max_length=400)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.title
