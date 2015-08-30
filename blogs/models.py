23d99c038b8be028737df2c510b09fd205a8fc6d
Jose Enrique Sanchez Alfonso <jesanchez@jesanchez.es>
2015-08-30 11:43:33 +0200

    Añadido usuario al blog y modificada plantilla del blog

--- blogs/models.py
+++ blogs/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Blog(models.Model):
 
+    owner = models.OneToOneField(User)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
