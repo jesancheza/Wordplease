a606fb85ddb6c94e13118dc780d9440dd3d92d76
Jose Enrique Sanchez Alfonso <jesanchez@jesanchez.es>
2015-08-30 12:04:21 +0200

    Nombrado de urls

--- wordplease/urls.py
+++ wordplease/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', 'blogs.views.home')
+    url(r'^$', 'blogs.views.home', name='wordplease_home')
 ]
