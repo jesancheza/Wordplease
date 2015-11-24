1c2233a36a500e5a51c3847a2900bf8f196c7041
Jose Enrique Sanchez Alfonso <jesanchez@jesanchez.es>
2015-11-24 23:53:58 +0100

    Add settings default

---
+++ wordplease/settings_develpe.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+__author__ = 'joseenriquesanchezalfonso'
+from settings import *
+
+# Logs all SQL queries in debug
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
