dea7f53eef203928ad18e4774c30d94e5bf1af9e
Jose Enrique Sanchez Alfonso <jesanchez@jesanchez.es>
2016-11-07 00:59:01 +0100

    Añadido filtro por categorías en el admin

--- posts/admin.py
+++ posts/admin.py
@@ -6,6 +6,7 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ('category__title', )
     list_display = ('title', 'image_url', 'summary', 'date_published')
     ordering = ('-date_published',)
+    list_filter = ('category', )
 
 
 class CategoryAdmin(admin.ModelAdmin):
